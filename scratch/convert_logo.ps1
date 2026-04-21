1d4d903395b875032ce84171e0c9ff66a6123936
SaadAbdullah72 <saad489254@gmail.com>
2026-04-21 09:55:13 +0500

    fix(compliance): generate resized icons to match manifest declarations

--- scratch/convert_logo.ps1
+++ scratch/convert_logo.ps1
@@ -1,10 +1,40 @@
+param (
+    [string]$sourceFile = "c:\Users\Hp\Desktop\e commerce\client\public\logo.png",
+    [string]$destinationDir = "c:\Users\Hp\Desktop\e commerce\client\public\"
+)
+
 Add-Type -AssemblyName System.Drawing
-$image = [System.Drawing.Image]::FromFile('c:\Users\Hp\Desktop\e commerce\client\public\logo.png')
-$bitmap = New-Object System.Drawing.Bitmap($image.Width, $image.Height)
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphics.DrawImage($image, 0, 0, $image.Width, $image.Height)
-$bitmap.Save('c:\Users\Hp\Desktop\e commerce\client\public\logo_converted.png', [System.Drawing.Imaging.ImageFormat]::Png)
-$graphics.Dispose()
-$bitmap.Dispose()
-$image.Dispose()
-Move-Item -Force 'c:\Users\Hp\Desktop\e commerce\client\public\logo_converted.png' 'c:\Users\Hp\Desktop\e commerce\client\public\logo.png'
+
+function Resize-Image {
+    param (
+        [string]$Path,
+        [string]$Destination,
+        [int]$Width,
+        [int]$Height
+    )
+    
+    $img = [System.Drawing.Image]::FromFile($Path)
+    $thumbnail = new-object System.Drawing.Bitmap($Width, $Height)
+    $graphic = [System.Drawing.Graphics]::FromImage($thumbnail)
+    
+    $graphic.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    $graphic.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+    $graphic.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
+    $graphic.CompositingQuality = [System.Drawing.Drawing2D.CompositingQuality]::HighQuality
+    
+    $graphic.DrawImage($img, 0, 0, $Width, $Height)
+    
+    $thumbnail.Save($Destination, [System.Drawing.Imaging.ImageFormat]::Png)
+    
+    $graphic.Dispose()
+    $thumbnail.Dispose()
+    $img.Dispose()
+}
+
+write-host "Resizing to 192x192..."
+Resize-Image -Path $sourceFile -Destination "$($destinationDir)logo-192.png" -Width 192 -Height 192
+
+write-host "Resizing to 512x512..."
+Resize-Image -Path $sourceFile -Destination "$($destinationDir)logo-512.png" -Width 512 -Height 512
+
+write-host "Done."
